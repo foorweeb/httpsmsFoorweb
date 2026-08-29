14e84e032c3e9549eb9ebfecfacb467250a5bfbd
Foorweb <63014064+foorweeb@users.noreply.github.com>
2026-08-29 17:56:17 +0100

    Push init

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,12 +10,12 @@ val gitHash = providers.exec {
 }.standardOutput.asText.map { it.trim() }
 
 android {
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.httpsms"
         minSdk = 28
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = gitHash.getOrElse("unknown")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
